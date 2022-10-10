07e6c5758231e16cbf4ef5b1e2c65894bcd85859
S.A.S.A <saro.ironi@hotmail.com>
2022-10-10 15:30:41 +0400

    Add App

---
+++ app/src/main/java/com/vmware/tanzu/demo/SpringBootOnKubernetesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vmware.tanzu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOnKubernetesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOnKubernetesDemoApplication.class, args);
+	}
+
+}
